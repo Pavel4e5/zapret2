c80efcc9835799492b59aada53a153a63da16ac6
bol-van <none@none.none>
2025-11-23 16:09:11 +0300

    zapret-tests.lua: aes_ctr const tests

--- nfq2/crypto/aes-ctr.h
+++ nfq2/crypto/aes-ctr.h
@@ -5,4 +5,3 @@
 
 void aes_ctr_xcrypt_buffer(aes_context *ctx, const uint8_t *iv, const uint8_t *in, size_t length, uint8_t *out);
 int aes_ctr_crypt(const uint8_t *key, unsigned int key_len, const uint8_t *iv, const uint8_t *in, size_t length, uint8_t *out);
-
